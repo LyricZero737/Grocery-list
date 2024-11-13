c9940c36fe4307ed7a123140cfc54d27a316b94e
LyricZero737 <lyric12705@gmail.com>
2024-11-13 16:32:44 -0600

    Add files via upload

---
+++ js/Code2.js
@@ -0,0 +1,34 @@
+var listItems = document.getElementsByTagName('li');                   // All <li> elements
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var heading = document.querySelector('h2');                        // h2 element
+var headingText = heading.firstChild.nodeValue;                    // h2 text
+var totalItems = listItems.length;                                 // No. of <li> elements
+var newHeading = headingText + '<span>' + totalItems + '</span>'; // Content
+
+
+/*  WRITE YOUR CODE BELOW */
+heading.innerHTML = newHeading;
+
+// Part1. Hint: Use the listItems object to iterate through the elements of the list
+
+for (var i = 0; i < totalItems; i++) {
+    if (listItems[i].innerHTML == "fresh figs") {
+        listItems[i].classList = 'cool';
+    } else if (listItems[i].innerHTML == "kale") {
+        listItems[i].className = 'cool';
+    } else if (listItems[i].innerHTML == "honey") {
+        listItems[i].className = 'cool';
+    }
+}
+
+
+
+// Part2. Hint: Use the heading and newHeading to show the number of items in the shopping list
+
+
+
+
+
+
+
